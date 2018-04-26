6b66b2d7a238f64d5a8446e4e58c9dd399be7729
Julie <Juliapretzlaff@gmail.com>
2018-04-26 19:07:50 -0400

    added JS event listener

--- StaticPageforJSON/JSsheet.js
+++ StaticPageforJSON/JSsheet.js
@@ -1,4 +1,29 @@
-﻿
+﻿var animals = document.getElementById("animals");
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+
+    var ourRequest = new XMLHttpRequest();
+    //first arg = send or recieve,  second = url from which we get JSON info
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+
+    //onload method.  what happens when data is loaded
+    ourRequest.onload = function () {
+        //test
+        //console.log(ourRequest.responseText)
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0])
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+});
+
+//function to which we'll pass our data
+function renderHTML(data) {
+    var htmlString = "test"
+    animals.insertAdjacentHTML('beforeend', htmlString);
+}
+
 ////JSON object
 //var myCat = {
 //    "name": "BearCat",
