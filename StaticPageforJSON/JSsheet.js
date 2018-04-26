6ca1a7456f8568891fdc59b6387d27cf8b1f5a3e
Julie <Juliapretzlaff@gmail.com>
2018-04-26 11:15:22 -0400

    added JSON/AJAX request

---
+++ StaticPageforJSON/JSsheet.js
@@ -0,0 +1,39 @@
+﻿
+////JSON object
+//var myCat = {
+//    "name": "BearCat",
+//    "species": "cat",
+//    "favorite activity" : "meowing"
+//}
+
+////JSON array
+//var myFavAnimals = ["dog", "dolphin", "owl", "cat"];
+
+
+////JSON array of objects
+//var myPets = [
+//    {
+//        "name": "BearCat",
+//        "species": "cat",
+//        "favorite activity": "meowing"
+//    },
+//    {
+//        "name": "Daisy Mae",
+//        "species": "dog",
+//        "favorite activity": "fetch"
+//    }
+//]
+
+var ourRequest = new XMLHttpRequest();
+//first arg = send or recieve,  second = url from which we get JSON info
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+
+//onload method.  what happens when data is loaded
+ourRequest.onload = function () {
+    //test
+    //console.log(ourRequest.responseText)
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0])
+
+};
+ourRequest.send();
